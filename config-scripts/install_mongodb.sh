9466db9d2b97a766c68d93b17cafa7f81167815b
Anton Voronov <avoronov@at-consulting.ru>
2021-03-24 20:55:42 +0300

    Revert "Add provisioners to pattern packer and delete sudo from scripts"
    This reverts commit 8cac5a4d2cb51deac13c950007a83dcbd76660a1.

--- config-scripts/install_mongodb.sh
+++ config-scripts/install_mongodb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
-apt-get update
-apt-get install -y mongodb-org
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
-systemctl start mongod
-systemctl enable mongod
+sudo systemctl start mongod
+sudo systemctl enable mongod
 
