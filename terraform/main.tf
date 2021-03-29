043f3be16f8cdf41b7c6e88db207a5f592308acc
Anton Voronov <avoronov@at-consulting.ru>
2021-03-29 11:42:45 +0300

    Install terraform 0.12 with yandex provider and change gitignor

---
+++ terraform/main.tf
@@ -0,0 +1,7 @@
+provider "yandex" {
+  token     = "/root/key.json"
+  cloud_id  = "b1gh1vbt756kp5uu9abr"
+  folder_id = "b1g593l2rokl9tfe4k8i"
+  zone      = "ru-central1-a"
+  version   = "~> 0.35"
+}
