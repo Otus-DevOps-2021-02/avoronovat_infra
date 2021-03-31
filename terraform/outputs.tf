c4e31519ce6fe64d0d15c03d73aa9f999955d934
Anton Voronov <avoronov@at-consulting.ru>
2021-03-31 11:55:14 +0300

    Add second app instance

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "external_ip_address_app" {
   value = yandex_compute_instance.app.network_interface.0.nat_ip_address
 }
 
+output "external_ip_address_app2" {
+  value = yandex_compute_instance.app2.network_interface.0.nat_ip_address
+}
+
 output "external_ip_balancer" {
   value = yandex_lb_network_load_balancer.balancer.listener.*.external_address_spec
 }
