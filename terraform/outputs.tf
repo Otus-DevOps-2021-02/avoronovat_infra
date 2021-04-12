fd58dc81d8a2740f22e64b432511ed6b11e1b43d
Anton Voronov <avoronov@at-consulting.ru>
2021-04-12 14:29:03 +0300

    add new resources - vpc network and subnet

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "external_ip_address_app" {
   value = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
 }
-
-output "external_ip_balancer" {
-  value = yandex_lb_network_load_balancer.balancer[0].listener.*.external_address_spec
-}
