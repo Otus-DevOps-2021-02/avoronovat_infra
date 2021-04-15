282e92c8f535aa0c0acc38d46635da0e7c2c0102
Anton Voronov <avoronov@at-consulting.ru>
2021-04-15 12:26:08 +0300

    add dynamic inventory.json from terraform tmpl

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -4,3 +4,25 @@ output "external_ip_address_app" {
 output "external_ip_address_db" {
   value = module.db.external_ip_address_db
 }
+
+### The Ansible inventory file to ansible dir
+resource "local_file" "AnsibleInventoryYML" {
+ content = templatefile("inventory.yml.tmpl",
+ {
+  app-ip = module.app.external_ip_address_app,
+  db-ip = module.db.external_ip_address_db
+ }
+ )
+ filename = "../../ansible/inventory.yml"
+}
+
+resource "local_file" "AnsibleInventoryJSON" {
+ content = templatefile("inventory.json.tmpl",
+ {
+  app-ip = module.app.external_ip_address_app,
+  db-ip = module.db.external_ip_address_db
+ }
+ )
+ filename = "../../ansible/inventory.json"
+}
+
